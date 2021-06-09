944addb69cf1a3e7dbdae66400fc3a800da6785d
Uadson <uadsoncastro@gmail.com>
2021-06-09 13:12:10 -0300

    criado class pessoa

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pessoa(models.Model):
+	nome = models.CharField(max_length=200, blank=False)
+
+	class Meta:
+		db_table = 'pessoa'
+
+	def __str__(self):
+		return self.nome
